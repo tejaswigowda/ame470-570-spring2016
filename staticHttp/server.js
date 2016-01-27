b3fa9a54f7015a73caefc6cec4761e5fc12ce6b2
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-01-27 09:46:02 -0700

    in class

--- staticHttp/server.js
+++ staticHttp/server.js
@@ -12,6 +12,7 @@ app.get("/", function (req, res) {
 
 app.get("/eval", function (req, res) {
     console.log(req.query);
+    var userInput = req.query.code;
     var result  = 0;
     // calculate res here
     //
