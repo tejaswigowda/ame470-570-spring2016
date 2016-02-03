1e2cd2efcc4b68a25ba71b349a2f0ea3c65e5ced
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-02-03 09:53:31 -0700

    in class

---
+++ jsonEg/server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8080;
+
+var exObj = {
+  course: "AME470/570",
+  startDate: "Jan 11, 2016",
+  endDate:"May 4, 2016"
+};
+
+app.get("/", function (req, res) {
+      res.redirect("/index.html");
+});
+
+app.get("/getJSObj", function (req, res) {
+     res.send(JSON.stringify(exObj))// send response body
+});
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
