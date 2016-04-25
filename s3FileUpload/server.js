09a5abc2aff25a0ca3a97451e72aa0bc1902bea7
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-04-25 09:56:43 -0700

    in class

--- s3FileUpload/server.js
+++ s3FileUpload/server.js
@@ -1,3 +1,8 @@
+fs = require('fs');
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./credentials.json');
+var s3 = new AWS.S3();
+
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
@@ -9,6 +14,28 @@ var port = 8080;
 app.get("/", function (req, res) {
       res.redirect("/index.html");
 });
+
+app.post('/uploadFile', isLoggedIn, function(req, res){
+        var intname = req.body.fileInput;
+        var filename = req.files.input.name;
+        var fileType =  req.files.input.type;
+        var tmpPath = req.files.input.path;
+        var s3Path = '/' + intname;
+        
+        fs.readFile(tmpPath, function (err, data) {
+            var params = {
+                Bucket:'alldone',
+                ACL:'public-read',
+                Key:intname,
+                Body: data,
+                ServerSideEncryption : 'AES256'
+            };
+            s3.putObject(params, function(err, data) {
+                console.log(err);
+                res.end("success");
+            });
+        });
+  });
 
 app.use(methodOverride());
 app.use(bodyParser());
