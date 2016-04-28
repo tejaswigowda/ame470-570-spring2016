5c6e61a7c775e2281da76c4e42ec253bbcbcc2ca
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-04-28 01:16:51 -0700

    works\!

--- s3FileUpload/server.js
+++ s3FileUpload/server.js
@@ -6,16 +6,31 @@ var s3 = new AWS.S3();
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8080;
+app.use(methodOverride());
+//app.use(bodyParser());
+app.use(require('connect').bodyParser());
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
 
 app.get("/", function (req, res) {
       res.redirect("/index.html");
 });
 
 app.post('/uploadFile', function(req, res){
+      console.log(req.body);
         var intname = req.body.fileInput;
         var filename = req.files.input.name;
         var fileType =  req.files.input.type;
@@ -24,7 +39,7 @@ app.post('/uploadFile', function(req, res){
         
         fs.readFile(tmpPath, function (err, data) {
             var params = {
-                Bucket:'alldone',
+                Bucket:'ame470',
                 ACL:'public-read',
                 Key:intname,
                 Body: data,
@@ -37,10 +52,6 @@ app.post('/uploadFile', function(req, res){
         });
   });
 
-app.use(methodOverride());
-app.use(bodyParser());
-app.use(express.static(__dirname + '/public'));
-app.use(errorHandler());
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
 app.listen(port);
